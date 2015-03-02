662232f173ec459d0c100a9fdeaac3c85e6562d4
Jonathan Camara <jcamara@ifma.org>
2015-03-02 09:52:10 -0600

    bumped to version 3.1.18.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'ManicTime' 'exe' '/quiet' 'http://cdn.manictime.com/setup/v3_0_5_0/ManicTime.exe'
+Install-ChocolateyPackage 'ManicTime' 'exe' '/quiet' 'http://cdn.manictime.com/setup/v3_1_18_0/ManicTime.exe'

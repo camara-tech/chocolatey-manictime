a0c834133db8a5cc63aa77b0c7cb4a31387a450d
Jonathan Camara <italy_portugal@yahoo.com>
2015-08-24 08:11:26 -0500

    Updated script for Manictime 3.2.2.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'ManicTime' 'exe' '/quiet' 'http://cdn.manictime.com/setup/v3_1_18_0/ManicTime.exe'
+Install-ChocolateyPackage 'ManicTime' 'exe' '/quiet' 'http://cdn.manictime.com/setup/v3_2_2_0/ManicTime.exe'
